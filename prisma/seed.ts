17fd376af11a3bd205299442c0925cded9dcbe0b
Huanyu Zhao <worldzhy@gmail.com>
2022-06-12 01:26:26 +0800

    build(seed): update seed

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -41,13 +41,55 @@ const projects: Prisma.ProjectCreateInput[] = [
           {
             type: ProjectEnvironmentType.DEVELOPMENT,
             awsProfile: 'Galaxy',
+            awsAccessKeyId: 'fakeAKIAREGsfXsf4FY',
+            awsSecretAccessKey: 'fakeGVxCF0AEbjdsftVsfswRR8V+uhQg8QJiZxMy',
             awsRegion: 'cn-northwest-1',
           },
-          {type: ProjectEnvironmentType.STAGING, awsRegion: 'cn-northwest-1'},
+          {
+            type: ProjectEnvironmentType.STAGING,
+            awsProfile: 'Galaxy',
+            awsAccessKeyId: 'fakeAKIAREGsfWUsfYEB4FY',
+            awsSecretAccessKey: 'fakeGVxCF0AEbj/xisfSsfwRR8V+uhQg8QJiZxMy',
+            awsRegion: 'cn-northwest-1',
+          },
           {
             type: ProjectEnvironmentType.PRODUCTION,
             awsProfile: 'Galaxy',
+            awsAccessKeyId: 'fakeAKsffsdWU5RXYEB4FY',
+            awsSecretAccessKey: 'fakeGVxCsfEbj/xi3asfvVswRR8V+uhQg8QJiZxMy',
             awsRegion: 'cn-northwest-1',
+          },
+        ],
+      },
+    },
+  },
+  {
+    name: 'InceptionPad',
+    status: ProjectStatus.IN_DEVELOPMENT,
+    environments: {
+      createMany: {
+        skipDuplicates: true,
+        data: [
+          {
+            type: ProjectEnvironmentType.DEVELOPMENT,
+            awsProfile: 'InceptionPad',
+            awsAccessKeyId: 'fakeAKIAQ734OFTUTLIKUV7D',
+            awsSecretAccessKey: 'fake7Nw/oOD34gsfoDBkec43s6B2ktqJj2MutiHyfwBUZ',
+            awsRegion: 'us-east-1',
+          },
+          {
+            type: ProjectEnvironmentType.STAGING,
+            awsProfile: 'InceptionPad',
+            awsAccessKeyId: 'fake434Q34OFsdfTLIKUV7D',
+            awsSecretAccessKey: 'fake7Nw/oOD34h345oafkecs6B2ktqJj2MsayfwBUZ',
+            awsRegion: 'us-east-1',
+          },
+          {
+            type: ProjectEnvironmentType.PRODUCTION,
+            awsProfile: 'InceptionPad',
+            awsAccessKeyId: 'fakeAKIAQ7IsOF23TLIKUV7D',
+            awsSecretAccessKey: 'fake7Nw/oODZ34Igf5oDsfcs6B2342MutiHyfwBUZ',
+            awsRegion: 'us-east-1',
           },
         ],
       },
