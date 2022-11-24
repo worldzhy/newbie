4350541e311846977a426d9056582165738a9d9d
worldzhy <worldzhy@126.com>
2022-11-24 05:50:17 +0000

    fix(recruitment): import workflow module in job application testing module

--- src/applications/recruitment/job-application/testing/testing.module.ts
+++ src/applications/recruitment/job-application/testing/testing.module.ts
@@ -2,9 +2,10 @@ import {Module} from '@nestjs/common';
 import {JobApplicationTestingWorkflowModule} from './workflow/testing-workflow.module';
 import {JobApplicationTestingController} from './testing.controller';
 import {JobApplicationTestingService} from './testing.service';
+import {WorkflowModule} from 'src/microservices/workflow/workflow.module';
 
 @Module({
-  imports: [JobApplicationTestingWorkflowModule],
+  imports: [JobApplicationTestingWorkflowModule, WorkflowModule],
   controllers: [JobApplicationTestingController],
   providers: [JobApplicationTestingService],
   exports: [JobApplicationTestingService],
