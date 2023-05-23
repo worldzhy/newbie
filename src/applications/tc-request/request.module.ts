ddd73a6d31b2cb3a34b1511d101ad57c4e2a3559
worldzhy <worldzhy@126.com>
2023-05-23 01:18:33 +0000

    fix(tc-request): delete payment controller

--- src/applications/tc-request/request.module.ts
+++ src/applications/tc-request/request.module.ts
@@ -2,14 +2,9 @@ import {Module} from '@nestjs/common';
 import {TcWorkflowModule} from './workflow/workflow.module';
 import {TcConstantController} from './constant/constant.controller';
 import {FileManagementController} from './fmgmt/fmgmt.controller';
-import {TcPaymentController} from './payment/payment.controller';
 
 @Module({
   imports: [TcWorkflowModule],
-  controllers: [
-    TcConstantController,
-    FileManagementController,
-    TcPaymentController,
-  ],
+  controllers: [TcConstantController, FileManagementController],
 })
 export class TcRequestModule {}
