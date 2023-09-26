2b3fe16ff5d61a56ad10d6acf49bc754001e53d5
worldzhy <worldzhy@126.com>
2023-09-26 03:42:58 +0000

    feat(event scheduling): add name field to availability expression

--- src/application/scheduling/event.controller.ts
+++ src/application/scheduling/event.controller.ts
@@ -18,13 +18,17 @@ export class EventController {
         summary: '1. Create mock data',
         value: {
           hostUserId: 'fd5c948e-d15d-48d6-a458-7798e4d9921c',
+          year: 2023,
+          month: 9,
         },
       },
     },
   })
-  async createMockData(@Body() body: {hostUserId: string}) {
+  async createMockData(
+    @Body() body: {hostUserId: string; year: number; month: number}
+  ) {
     console.log('- Creating events...');
-    for (let i = 0; i < 10; i++) {
+    for (let i = 0; i < 20; i++) {
       for (let j = 0; j < 10; j++) {
         await this.eventService.createMany({
           data: [
@@ -33,7 +37,7 @@ export class EventController {
               datetimeOfStart: '2023-09-01T06:00:00.000Z',
               datetimeOfEnd: '2023-09-01T06:50:00.000Z',
               year: 2023,
-              month: 9,
+              month: body.month,
               dayOfMonth: 1 + i,
               dayOfWeek: (5 + i) % 7,
               hour: 6 + j,
