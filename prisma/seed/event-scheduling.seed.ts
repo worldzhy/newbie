04cd14b00c17337b9b16385e6f41ed04c0b6ccec
worldzhy <worldzhy@126.com>
2023-10-24 03:15:41 +0000

    refactor(solidcore): remove useless seed data

--- prisma/seed/event-scheduling.seed.ts
+++ prisma/seed/event-scheduling.seed.ts
@@ -1,9 +1,7 @@
 import {PrismaClient} from '@prisma/client';
-import {prismaMiddleware} from '@toolkit/prisma/prisma.middleware';
 
 export async function seedForEventScheduling() {
   const prisma = new PrismaClient();
-  prisma.$use(prismaMiddleware);
 
   console.log('\n* Event Calendar Service');
 
@@ -57,102 +55,6 @@ export async function seedForEventScheduling() {
       {
         name: 'Advanced Full Body',
         minutesOfDuration: 65,
-      },
-    ],
-  });
-
-  console.log('- Creating event venues...');
-  await prisma.eventVenue.createMany({
-    data: [
-      {
-        name: 'CA, Culver City',
-        numberOfSeats: 20,
-        minutesOfBreak: 10,
-        placeId: 1,
-        external_studioId: 5723396,
-        external_studioName: '[solidcore] California',
-        external_locationId: 1,
-      },
-      {
-        name: 'CA, Hollywood',
-        numberOfSeats: 20,
-        minutesOfBreak: 10,
-        placeId: 2,
-        external_studioId: 5723396,
-        external_studioName: '[solidcore] California',
-        external_locationId: 2,
-      },
-      {
-        name: 'CA, West Hollywood',
-        numberOfSeats: 20,
-        minutesOfBreak: 10,
-        placeId: 3,
-        external_studioId: 5723396,
-        external_studioName: '[solidcore] California',
-        external_locationId: 3,
-      },
-      {
-        name: 'CA, Santa Monica',
-        numberOfSeats: 20,
-        minutesOfBreak: 10,
-        placeId: 4,
-        external_studioId: 5723396,
-        external_studioName: '[solidcore] California',
-        external_locationId: 4,
-      },
-    ],
-  });
-
-  console.log('- Creating event container...');
-  await prisma.eventContainer.createMany({
-    data: [
-      {
-        year: 2023,
-        month: 9,
-        timezone: 'Europe/Athens',
-        venueId: 1,
-      },
-      {
-        year: 2023,
-        month: 10,
-        timezone: 'Europe/Athens',
-        venueId: 1,
-      },
-      {
-        year: 2023,
-        month: 9,
-        timezone: 'Europe/Athens',
-        venueId: 2,
-      },
-      {
-        year: 2023,
-        month: 10,
-        timezone: 'Europe/Athens',
-        venueId: 2,
-      },
-      {
-        year: 2023,
-        month: 9,
-        timezone: 'Europe/Athens',
-        venueId: 3,
-      },
-      {
-        year: 2023,
-        month: 10,
-        timezone: 'Europe/Athens',
-        venueId: 3,
-      },
-      {
-        year: 2023,
-        month: 9,
-        timezone: 'Europe/Athens',
-        venueId: 4,
-      },
-      {
-        year: 2023,
-        month: 10,
-        timezone: 'Europe/Athens',
-        venueId: 4,
       },
     ],
   });
