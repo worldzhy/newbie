d68805422f84030fa14345972a1db5e350f4ccb7
worldzhy <worldzhy@126.com>
2024-03-28 07:23:47 +0000

    refactor(google drive): refine code

--- src/application0/file-mgmt/google-drive.controller.ts
+++ src/application0/file-mgmt/google-drive.controller.ts
@@ -1,12 +1,4 @@
-import {
-  Post,
-  Body,
-  Controller,
-  Delete,
-  Param,
-  Get,
-  Query,
-} from '@nestjs/common';
+import {Post, Body, Controller, Delete, Param} from '@nestjs/common';
 import {ApiTags, ApiBearerAuth, ApiBody} from '@nestjs/swagger';
 import {GoogleDriveService} from '@microservices/googleapis/drive/drive.service';
 import {GoogleAccountRole} from '@microservices/googleapis/enum';
@@ -16,6 +8,87 @@ import {GoogleAccountRole} from '@microservices/googleapis/enum';
 @Controller('google-drive')
 export class GoogleDriveController {
   constructor(private readonly googleDrive: GoogleDriveService) {}
+
+  @Post('files/folder')
+  @ApiBody({
+    description: '',
+    examples: {
+      a: {
+        summary: '1. Create',
+        value: {name: '', parentId: '[Optional]'},
+      },
+    },
+  })
+  async createFolder(@Body() body: {name: string; parentId?: string}) {
+    return this.googleDrive.createFolder(body);
+  }
+
+  @Post('files/document')
+  @ApiBody({
+    description: '',
+    examples: {
+      a: {
+        summary: '1. Create',
+        value: {name: '', parentId: '[Optional]'},
+      },
+    },
+  })
+  async createDocument(@Body() body: {name: string; parentId?: string}) {
+    return this.googleDrive.createDocument({
+      name: body.name,
+      parentId: body.parentId,
+    });
+  }
+
+  @Post('files/spreadsheet')
+  @ApiBody({
+    description: '',
+    examples: {
+      a: {
+        summary: '1. Create',
+        value: {name: '', parentId: '[Optional]'},
+      },
+    },
+  })
+  async createSpreadsheet(@Body() body: {name: string; parentId?: string}) {
+    return this.googleDrive.createSheet({
+      name: body.name,
+      parentId: body.parentId,
+    });
+  }
+
+  @Delete('files/:fileId')
+  async deleteFile(@Param('fileId') fileId: string) {
+    return await this.googleDrive.deleteFile(fileId);
+  }
+
+  @Post('list')
+  @ApiBody({
+    description: '',
+    examples: {
+      a: {
+        summary: '1. List',
+        value: {parentId: '[Optional]'},
+      },
+    },
+  })
+  async listFiles(@Body() body: {parentId?: string}) {
+    return await this.googleDrive.listFiles(body);
+  }
+
+  @Post('search')
+  @ApiBody({
+    description: '',
+    examples: {
+      a: {
+        summary: '1. Search',
+        value: {name: ''},
+      },
+    },
+  })
+  async searchFiles(@Body() body: {name: string}) {
+    return await this.googleDrive.searchFiles(body);
+  }
 
   @Post('share')
   @ApiBody({
@@ -36,92 +109,5 @@ export class GoogleDriveController {
   ) {
     return this.googleDrive.share(body);
   }
-
-  @Post('folders')
-  @ApiBody({
-    description: '',
-    examples: {
-      a: {
-        summary: '1. Create',
-        value: {name: '', parentId: '[Optional]'},
-      },
-    },
-  })
-  async createFolder(@Body() body: {name: string; parentId?: string}) {
-    return this.googleDrive.createFolder(body);
-  }
-
-  @Delete('folders/:fileId')
-  async deleteFolder(@Param('fileId') fileId: string) {
-    return await this.googleDrive.deleteFile(fileId);
-  }
-
-  @Post('search')
-  @ApiBody({
-    description: '',
-    examples: {
-      a: {
-        summary: '1. Search',
-        value: {name: ''},
-      },
-    },
-  })
-  async searchFiles(@Body() body: {name: string}) {
-    return await this.googleDrive.searchFiles(body);
-  }
-
-  @Post('list')
-  @ApiBody({
-    description: '',
-    examples: {
-      a: {
-        summary: '1. List',
-        value: {parentId: '[Optional]'},
-      },
-    },
-  })
-  async listFiles(@Body() body: {parentId?: string}) {
-    return await this.googleDrive.listFiles(body);
-  }
-
-  @Post('files/document')
-  @ApiBody({
-    description: '',
-    examples: {
-      a: {
-        summary: '1. Create',
-        value: {name: '', parentId: ''},
-      },
-    },
-  })
-  async createDocument(@Body() body: {name: string; parentId: string}) {
-    return this.googleDrive.createDocument({
-      name: body.name,
-      parentId: body.parentId,
-    });
-  }
-
-  @Post('files/spreadsheet')
-  @ApiBody({
-    description: '',
-    examples: {
-      a: {
-        summary: '1. Create',
-        value: {name: '', parentId: ''},
-      },
-    },
-  })
-  async createSpreadsheet(@Body() body: {name: string; parentId: string}) {
-    return this.googleDrive.createSheet({
-      name: body.name,
-      parentId: body.parentId,
-    });
-  }
-
-  @Delete('files/:fileId')
-  async deleteFile(@Param('fileId') fileId: string) {
-    return await this.googleDrive.deleteFile(fileId);
-  }
-
   /* End */
 }
