1d8799a064fe7c008c923d099ba16f1ea8d50ddc
zimv <yiwen@inceptionpad.com>
2024-04-23 14:22:59 +0800

    fix(people-finder): catch error res

--- src/microservices/people-finder/proxycurl/proxycurl.service.ts
+++ src/microservices/people-finder/proxycurl/proxycurl.service.ts
@@ -38,6 +38,22 @@ export class ProxycurlService {
     // });
   }
 
+  catchErrorRes = ({
+    error,
+    errorTitle,
+    resolve,
+  }: {
+    error: unknown;
+    errorTitle: string;
+    resolve: (error: object) => void;
+  }) => {
+    const resError = {error, spent: 0};
+    resolve({error: resError});
+    this.logger.error(
+      errorTitle + JSON.stringify(resError),
+      this.loggerContext
+    );
+  };
   /**
    * https://github.com/nubelaco/proxycurl-js-linkedin-profile-scraper/blob/main/docs/PeopleAPIApi.md
    * 1 credit
@@ -49,43 +65,51 @@ export class ProxycurlService {
     location,
   }: SearchPeopleLinkedinReqDto): Promise<SearchPeopleLinkedinResDto> {
     return new Promise(resolve => {
-      this.api.personLookupEndpoint(
-        domain,
-        firstName,
-        {
-          location,
-          // title
-          lastName,
-        },
-        (error, data, response) => {
-          let spent = 0;
-          if (response.header['x-proxycurl-credit-cost']) {
-            try {
-              spent = Number(response.header['x-proxycurl-credit-cost']);
-            } catch (e) {
-              spent = 0;
+      try {
+        this.api.personLookupEndpoint(
+          domain,
+          firstName,
+          {
+            location,
+            // title
+            lastName,
+          },
+          (error, data, response) => {
+            let spent = 0;
+            if (response.header['x-proxycurl-credit-cost']) {
+              try {
+                spent = Number(response.header['x-proxycurl-credit-cost']);
+              } catch (e) {
+                spent = 0;
+              }
+            }
+            // data {url:string}
+            if (error) {
+              const resError = {error};
+              resolve({error: resError, spent});
+              this.logger.error(
+                'Proxycurl searchPeopleLinkedin error: ' +
+                  JSON.stringify(resError),
+                this.loggerContext
+              );
+            } else {
+              // response.body has more details
+              resolve({res: response.body, spent});
+              this.logger.log(
+                'Proxycurl searchPeopleLinkedin success: ' +
+                  JSON.stringify(response.body),
+                this.loggerContext
+              );
             }
           }
-          // data {url:string}
-          if (error) {
-            const resError = {error};
-            resolve({error: resError, spent});
-            this.logger.error(
-              'Proxycurl searchPeopleLinkedin error: ' +
-                JSON.stringify(resError),
-              this.loggerContext
-            );
-          } else {
-            // response.body has more details
-            resolve({res: response.body, spent});
-            this.logger.log(
-              'Proxycurl searchPeopleLinkedin success: ' +
-                JSON.stringify(response.body),
-              this.loggerContext
-            );
-          }
-        }
-      );
+        );
+      } catch (error) {
+        this.catchErrorRes({
+          error,
+          errorTitle: 'Proxycurl searchPeopleLinkedin error: ',
+          resolve,
+        });
+      }
     });
   }
 
@@ -100,42 +124,50 @@ export class ProxycurlService {
     personalContactNumber,
   }: SearchPeopleByLinkedinReqDto): Promise<SearchPeopleByLinkedinResDto> {
     return new Promise(resolve => {
-      this.api.personProfileEndpoint(
-        linkedinUrl,
-        'on-error',
-        {
-          // Costs an extra `1` credit per email returned on top of the cost of the base endpoint (if data is available).
-          personalEmail,
-          // Costs an extra `1` credit per number returned on top of the cost of the base endpoint (if data is available).
-          personalContactNumber,
-        },
-        (error, data: SearchPeopleByLinkedinRes, response) => {
-          let spent = 0;
-          if (response.header['x-proxycurl-credit-cost']) {
-            try {
-              spent = Number(response.header['x-proxycurl-credit-cost']);
-            } catch (e) {
-              spent = 0;
+      try {
+        this.api.personProfileEndpoint(
+          linkedinUrl,
+          'on-error',
+          {
+            // Costs an extra `1` credit per email returned on top of the cost of the base endpoint (if data is available).
+            personalEmail,
+            // Costs an extra `1` credit per number returned on top of the cost of the base endpoint (if data is available).
+            personalContactNumber,
+          },
+          (error, data: SearchPeopleByLinkedinRes, response) => {
+            let spent = 0;
+            if (response.header['x-proxycurl-credit-cost']) {
+              try {
+                spent = Number(response.header['x-proxycurl-credit-cost']);
+              } catch (e) {
+                spent = 0;
+              }
+            }
+            if (error) {
+              const resError = {error};
+              resolve({error: resError, spent});
+              this.logger.error(
+                'Proxycurl searchPeopleByLinkedin error: ' +
+                  JSON.stringify(resError),
+                this.loggerContext
+              );
+            } else {
+              resolve({res: data, spent});
+              this.logger.log(
+                'Proxycurl searchPeopleByLinkedin success: ' +
+                  JSON.stringify(data),
+                this.loggerContext
+              );
             }
           }
-          if (error) {
-            const resError = {error};
-            resolve({error: resError, spent});
-            this.logger.error(
-              'Proxycurl searchPeopleByLinkedin error: ' +
-                JSON.stringify(resError),
-              this.loggerContext
-            );
-          } else {
-            resolve({res: data, spent});
-            this.logger.log(
-              'Proxycurl searchPeopleByLinkedin success: ' +
-                JSON.stringify(data),
-              this.loggerContext
-            );
-          }
-        }
-      );
+        );
+      } catch (error) {
+        this.catchErrorRes({
+          error,
+          errorTitle: 'Proxycurl searchPeopleByLinkedin error: ',
+          resolve,
+        });
+      }
     });
   }
 }
