29e2a09002a3f5bc82b6f7380d2349ad87ab494d
zimv <yiwen@inceptionpad.com>
2024-05-29 18:11:17 +0800

    chore(notification): remove annotation

--- src/microservices/notification/webhook/feishu/constants.ts
+++ src/microservices/notification/webhook/feishu/constants.ts
@@ -3,14 +3,9 @@ export enum FeishuWebhookStatus {
 }
 
 export enum FeishuWebhookMsgType {
-  /** 文本 */
   text = 'text',
-  /** 富文本 */
   post = 'post',
-  /** 群名片 */
   shareChat = 'share_chat',
-  /** 图片 */
   image = 'image',
-  /** 消息卡片 */
   interactive = 'interactive',
 }
