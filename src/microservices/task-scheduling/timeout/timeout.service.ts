48376a84ef25808a806f895568bd72044e398f6a
worldzhy <11963092+worldzhy@users.noreply.github.com>
2024-07-29 03:30:26 +0000

    refactor(microservices): cron -> task-scheduling

---
+++ src/microservices/task-scheduling/timeout/timeout.service.ts
@@ -0,0 +1,29 @@
+import {Injectable} from '@nestjs/common';
+import {SchedulerRegistry} from '@nestjs/schedule';
+
+@Injectable()
+export class TimeoutTaskService {
+  constructor(private schedulerRegistry: SchedulerRegistry) {}
+
+  listTasks() {
+    return this.schedulerRegistry.getTimeouts();
+  }
+
+  createTask(params: {
+    name: string;
+    milliseconds: number;
+    callback: () => Promise<void>;
+  }) {
+    const timeout = setTimeout(params.callback, params.milliseconds);
+    this.schedulerRegistry.addTimeout(params.name, timeout);
+
+    console.log('A timeout task ' + params.name + ' has been created.');
+  }
+
+  deleteTask(name: string) {
+    this.schedulerRegistry.deleteTimeout(name);
+    console.warn(`A timeout task ${name} has been deleted.`);
+  }
+
+  /* End */
+}
