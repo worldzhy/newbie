3a0f08a42a8a7a556943753df7b2371311ab9f23
zimv <yiwen@inceptionpad.com>
2025-01-17 21:49:07 +0800

    refactor: account

--- src/microservices/microservices.module.ts
+++ src/microservices/microservices.module.ts
@@ -1,7 +1,19 @@
 import {Global, Module} from '@nestjs/common';
+import MicroservicesConfiguration from './microservices.config';
+import {ConfigModule} from '@nestjs/config';
+import {AccountModule} from './account/account.module';
+import {ElasticsearchModule} from './elasticsearch/elasticsearch.module';
+import {MessageBotModule} from './message-bot/message-bot.module';
+import {NotificationModule} from './notification/notification.module';
 
 @Global()
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({load: [MicroservicesConfiguration], isGlobal: true}),
+    AccountModule,
+    ElasticsearchModule,
+    MessageBotModule,
+    NotificationModule,
+  ],
 })
 export class MicroservicesModule {}
