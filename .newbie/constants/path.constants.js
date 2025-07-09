a769bb6f3cdbff5faedadf741d563c1457a77d5e
worldzhy <worldzhy@126.com>
2025-07-09 18:40:47 +0000

    fix(newbie): fix generate .env.example failed

--- .newbie/constants/path.constants.js
+++ .newbie/constants/path.constants.js
@@ -11,6 +11,7 @@ const PRISMA_SCHEMA_MAIN_PATH = './prisma/schema.prisma';
 const PRISMA_SCHEMA_MODELS_PATH = './prisma/models';
 
 const ENV_PATH = './.env';
+const ENV_EXAMPLE_PATH = './.env.example';
 const GIT_MODULES = '.gitmodules';
 const NEST_CLI_PATH = './nest-cli.json';
 
@@ -28,6 +29,7 @@ module.exports = {
   PRISMA_SCHEMA_MODELS_PATH,
 
   ENV_PATH,
+  ENV_EXAMPLE_PATH,
   GIT_MODULES,
   NEST_CLI_PATH,
 };
