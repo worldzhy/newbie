366b442e771fdc442977cd9e8fc85d38b2d1ff27
Yu Wen <wenyu@inceptionpad.com>
2026-01-13 14:44:58 +0800

    refactor: change PrismaService to factory provider pattern

--- src/framework/prisma/prisma.module.ts
+++ src/framework/prisma/prisma.module.ts
@@ -1,9 +1,31 @@
 import {Global, Module} from '@nestjs/common';
-import {PrismaService} from '@framework/prisma/prisma.service';
+import {PrismaService} from './prisma.service';
+import {createExtendedPrismaClient} from './prisma.extension';
 
 @Global()
 @Module({
-  providers: [PrismaService],
+  providers: [
+    {
+      provide: PrismaService,
+      useFactory: async () => {
+        const client = createExtendedPrismaClient();
+
+        /**
+         * Manually bind NestJS lifecycle hooks to the extended Prisma client.
+         * Since the extended client is a plain object, we attach the hooks directly.
+         */
+        (client as any).onModuleInit = async () => {
+          await (client as any).$connect();
+        };
+
+        (client as any).onModuleDestroy = async () => {
+          await (client as any).$disconnect();
+        };
+
+        return client;
+      },
+    },
+  ],
   exports: [PrismaService],
 })
 export class PrismaModule {}
